6e12b8a674618450a2a22cd327da1c28903f6253
i.vukolov <wukolov-ilya@yandex.ru>
2025-02-16 21:35:25 +0300

    added class for detail view

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
@@ -61,6 +63,19 @@ class BirthdayListView(ListView):
     paginate_by = 2
 
 
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
 
 
 # Аналог класса BirthdayListView в виде функции.
