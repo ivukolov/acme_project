e13f023d936b5306c6272521923355dfe24ee068
i.vukolov <wukolov-ilya@yandex.ru>
2025-02-16 19:25:08 +0300

    added static content

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
     import debug_toolbar
     # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
