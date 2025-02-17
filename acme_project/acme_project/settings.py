cc3cb59df8159f79846339410a6f150582b9c5f4
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-17 17:08:27 +0300

    added users app

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
+    'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'debug_toolbar',
 ]
