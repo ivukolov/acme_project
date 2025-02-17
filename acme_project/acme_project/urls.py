d837465ab3a713205acfa69020c28c9ad3b132b1
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-17 15:42:59 +0300

    added register form

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
+from django.urls import include, path, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('auth/registration/',
+         CreateView.as_view(
+             template_name='registration/registration_form.html',
+             form_class=UserCreationForm,
+             success_url=reverse_lazy('pages:homepage'),
+         ),
+         name='registration',
+         ),
     path('auth/', include('django.contrib.auth.urls')),
     path('birthday/', include('birthday.urls')),
 ]
@@ -14,4 +25,5 @@ if settings.DEBUG:
     import debug_toolbar
     # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
