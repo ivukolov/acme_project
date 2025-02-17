68541f8fa4177301b9f9915609ae122043569573
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-17 17:09:17 +0300

    added LoginRequiredMixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,6 +2,7 @@ from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import BirthdayForm
 from .models import Birthday
@@ -76,17 +77,17 @@ class BirthdayListView(ListView):
 
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayUpdateView(UpdateView):
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
