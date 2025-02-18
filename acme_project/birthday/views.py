3d1e5b525cb51ce9d9f5586b8bbb04f49ec476c1
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-18 13:12:05 +0300

    added CBV Congratulation

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,12 +1,14 @@
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import get_object_or_404, redirect
 
-from .forms import BirthdayForm
-from .models import Birthday
+from .forms import BirthdayForm, CongratulationForm
+from .models import Birthday, Congratulation
 from .utils import calculate_birthday_countdown
 
 # def birthday(request, pk=None):
@@ -69,22 +71,44 @@ from .utils import calculate_birthday_countdown
 #     # Если был получен GET-запрос — отображаем форму.
 #     return render(request, 'birthday/birthday.html', context)
 
+
+# Будут обработаны POST-запросы только от залогиненных пользователей.
+# @login_required
+# def add_comment(request, pk):
+#     # Получаем объект дня рождения или выбрасываем 404 ошибку.
+#     birthday = get_object_or_404(Birthday, pk=pk)
+#     # Функция должна обрабатывать только POST-запросы.
+#     form = CongratulationForm(request.POST)
+#     if form.is_valid():
+#         # Создаём объект поздравления, но не сохраняем его в БД.
+#         congratulation = form.save(commit=False)
+#         # В поле author передаём объект автора поздравления.
+#         congratulation.author = request.user
+#         # В поле birthday передаём объект дня рождения.
+#         congratulation.birthday = birthday
+#         # Сохраняем объект в БД.
+#         congratulation.save()
+#     # Перенаправляем пользователя назад, на страницу дня рождения.
+#     return redirect('birthday:detail', pk=pk)
+
+
 class OnlyAuthorMixin(UserPassesTestMixin):
 
     def test_func(self):
         object = self.get_object()
-        return object.author == self.request.user 
+        return object.author == self.request.user
 
 
 class BirthdayListView(ListView):
     model = Birthday
+    queryset = Birthday.objects.prefetch_related(
+        'tags'
+    ).select_related('author')
     ordering = 'id'
     paginate_by = 10
 
 
-
-
-class BirthdayCreateView(LoginRequiredMixin, CreateView):
+class BirthdayCreateView(CreateView):
     model = Birthday
     form_class = BirthdayForm
 
@@ -95,12 +119,12 @@ class BirthdayCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class BirthdayUpdateView(LoginRequiredMixin, UpdateView, OnlyAuthorMixin):
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(LoginRequiredMixin, DeleteView, OnlyAuthorMixin):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
@@ -113,4 +137,33 @@ class BirthdayDetailView(DetailView):
         context['birthday_countdown'] = calculate_birthday_countdown(
             self.object.birthday
         )
+        # Записываем в переменную form пустой объект формы.
+        context['form'] = CongratulationForm()
+        # Запрашиваем все поздравления для выбранного дня рождения.
+        context['congratulations'] = (
+            # Дополнительно подгружаем авторов комментариев,
+            # чтобы избежать множества запросов к БД.
+            self.object.congratulations.select_related('author')
+        )
         return context
+
+
+class CongratulationCreateView(OnlyAuthorMixin, CreateView):
+    birthday = None
+    model = Congratulation
+    form_class = CongratulationForm
+
+    # Переопределяем dispatch()
+    def dispatch(self, request, *args, **kwargs):
+        self.birthday = get_object_or_404(Birthday, pk=kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+
+    # Переопределяем form_valid()
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.birthday = self.birthday
+        return super().form_valid(form)
+
+    # Переопределяем get_success_url()
+    def get_success_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.birthday.pk}) 
